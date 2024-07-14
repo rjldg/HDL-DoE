3a33b65d8e9f1be584da812e33cf4c1d2f750a19
rjldg <rjlimdeguzman@gmail.com>
2024-07-14 22:57:46 +0800

    Tested demux with connection to input prio encoder

--- main_tb.v
+++ main_tb.v
@@ -4,9 +4,12 @@
 module main_tb;
 
     wire [4:0] out;
+    wire [1:0] mode_out;
     reg [9:0] x;
+    reg sel;
 
     input_encoder enc(x, out);
+    demux1_2 dmx(mode_out, out[4], sel);
 
     initial begin
         $dumpfile("main_tb.vcd"); $dumpvars(0, main_tb);
@@ -14,9 +17,11 @@ module main_tb;
         x[0]=0; x[1]=0; x[2]=0; x[3]=0; x[4]=0; 
         x[5]=0; x[6]=0; x[7]=0; x[8]=0; x[9]=0;
 
+        sel = 1'b0;
+
         $display("*** SIMULATING INPUT ENCODER ***");
-        $display("\t BCD Output\t   Keypad Input");
-        $monitor("\t   %b\t    %b", out, x);
+        $display("\t BCD Output\t   Keypad Input\t    Demux Output");
+        $monitor("\t   %b\t    %b\t         %b", out, x, mode_out);
 
         #5 x[0]=1;
         #5 x[1]=1;
